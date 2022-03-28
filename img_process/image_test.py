283430d8a403e20618a3ac9acdfe058eb6690c2e
Sesim <sesim@atamisk.net>
2022-03-28 16:37:02 -0400

    Added checking if the space is lit!

--- img_process/image_test.py
+++ img_process/image_test.py
@@ -8,7 +8,12 @@ if __name__ == "__main__":
 
     grid = grid_gen.make_grid([37.5,36.5], -1.5*pi/180, [835,435])
     for pt in grid: 
-        draw.regular_polygon((*pt, 8), 3, rotation=30, fill=(255,0,0))
+        pixel_value = im.getpixel(tuple(pt))
+        pixel_norm = sum(pixel_value)
+        print("{} -- {}".format(pixel_value, pixel_norm))
+        color = (255,0,0) if pixel_norm > 500 else (255,255,0)
+        draw.regular_polygon((*pt, 8), 3, rotation=30, fill=color)
 
+    im.save("/tmp/alignment-test.jpg")
     im.show()
 
